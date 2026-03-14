8b0176062a3e258b253e593074f925e2c54cdc6f
D1MANYCH <101007426+D1MANYCH@users.noreply.github.com>
2026-03-15 03:16:41 +0500

    Update CACHE_NAME to version 2

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 // sw.js — Service Worker для офлайн-работы D&D Sheet
 // ============================================================
 
-const CACHE_NAME = 'dnd-sheet-v1';
+const CACHE_NAME = 'dnd-sheet-v2';
+
 
 const FILES_TO_CACHE = [
   './',
