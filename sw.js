d8539d6f8bd0da10f83b385c1e3f9111553c3449
D1MANYCH <KarginDmitriy@mail.ru>
2026-03-16 00:01:52 +0500

    Update sw

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // sw.js — Service Worker для офлайн-работы D&D Sheet
 // ============================================================
 
-const CACHE_NAME = 'dnd-sheet-v4';
+const CACHE_NAME = 'dnd-sheet-v5';
 
 const FILES_TO_CACHE = [
   './',
